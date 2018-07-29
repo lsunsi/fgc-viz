1e1cfd4e0c6049579688059364112ebc46591429
lsunsi <sunsi.lucas@gmail.com>
2018-07-29 17:08:08 -0300

    Add interest form logic coded in three message kinds

--- source/Update.elm
+++ source/Update.elm
@@ -1,12 +1,16 @@
 module Update exposing (Msg(..), update)
 
+import Date.Extra as Date
 import Json.Encode exposing (Value)
-import Model exposing (Model)
+import Model exposing (Interest, Model)
 import Storage exposing (decodeModel)
 
 
 type Msg
     = ModelRetrieved Value
+    | InterestFormDateInput String
+    | InterestFormRateInput String
+    | InterestFormSubmitted
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
@@ -14,3 +18,34 @@ update msg model =
     case msg of
         ModelRetrieved value ->
             Maybe.withDefault model (decodeModel value) ! []
+
+        InterestFormDateInput str ->
+            { model | interestFormDate = str } ! []
+
+        InterestFormRateInput str ->
+            { model | interestFormRate = str } ! []
+
+        InterestFormSubmitted ->
+            let
+                dateResult =
+                    Date.fromIsoString model.interestFormDate
+
+                rateResult =
+                    String.toFloat model.interestFormRate
+
+                withInterest interest =
+                    List.sortWith
+                        (\i1 i2 -> Date.compare i1.date i2.date)
+                        (interest :: model.interests)
+            in
+            case ( dateResult, rateResult ) of
+                ( Ok date, Ok rate ) ->
+                    { model
+                        | interests = withInterest (Interest date rate)
+                        , interestFormDate = ""
+                        , interestFormRate = ""
+                    }
+                        ! []
+
+                _ ->
+                    model ! []
--- tests/Test/Update.elm
+++ tests/Test/Update.elm
@@ -8,7 +8,7 @@ import Json.Encode as Encode
 import Model exposing (Interest, Model)
 import Test exposing (Test, describe, test)
 import Tuple exposing (first)
-import Update exposing (Msg(ModelRetrieved), update)
+import Update exposing (Msg(..), update)
 
 
 model0 : Model
@@ -48,4 +48,85 @@ modelRetrieved =
                 model0
                     |> update (ModelRetrieved (Encode.string "invalid"))
                     |> equal (model0 ! [])
+        ]
+
+
+interestFormDateInput : Test
+interestFormDateInput =
+    test "sets the interestFormDate field to message value" <|
+        \() ->
+            let
+                model1 =
+                    { model0 | interestFormDate = "some-date" }
+            in
+            model0
+                |> update (InterestFormDateInput "some-date")
+                |> equal (model1 ! [])
+
+
+interestFormRateInput : Test
+interestFormRateInput =
+    test "sets the interestFormRate field to message value" <|
+        \() ->
+            let
+                model1 =
+                    { model0 | interestFormRate = "some-rate" }
+            in
+            model0
+                |> update (InterestFormRateInput "some-rate")
+                |> equal (model1 ! [])
+
+
+interestFormSubmitted : Test
+interestFormSubmitted =
+    describe "InterestFormSubmitted"
+        [ test "creates interest when inputs are valid" <|
+            \() ->
+                let
+                    model1 =
+                        { model0
+                            | interests =
+                                [ Interest (Date.fromCalendarDate 2018 Date.Jan 10) 1.2
+                                , Interest (Date.fromCalendarDate 2018 Date.Jan 12) 1.2
+                                ]
+                            , interestFormDate = ""
+                            , interestFormRate = ""
+                        }
+
+                    model2 =
+                        { model1
+                            | interests =
+                                [ Interest (Date.fromCalendarDate 2018 Date.Jan 10) 1.2
+                                , Interest (Date.fromCalendarDate 2018 Date.Jan 11) 1.2
+                                , Interest (Date.fromCalendarDate 2018 Date.Jan 12) 1.2
+                                ]
+                        }
+                in
+                model1
+                    |> (update (InterestFormDateInput "2018-01-11") >> first)
+                    |> (update (InterestFormRateInput "1.2") >> first)
+                    |> update InterestFormSubmitted
+                    |> equal (model2 ! [])
+        , test "performs no operation when date input is invalid" <|
+            \() ->
+                let
+                    model1 =
+                        { model0 | interestFormDate = "invalid", interestFormRate = "1.2" }
+                in
+                model0
+                    |> (update (InterestFormDateInput "invalid") >> first)
+                    |> (update (InterestFormRateInput "1.2") >> first)
+                    |> update InterestFormSubmitted
+                    |> equal (model1 ! [])
+        , test "performs no operation when rate input is invalid" <|
+            \() ->
+                let
+                    model1 =
+                        { model0 | interestFormDate = "2018-01-11", interestFormRate = "invalid" }
+                in
+                model0
+                    |> (update (InterestFormDateInput "2018-01-11") >> first)
+                    |> (update (InterestFormRateInput "invalid") >> first)
+                    |> update InterestFormSubmitted
+                    |> equal (model1 ! [])
         ]
